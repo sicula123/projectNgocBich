9891c6a8d3c1071294972a8e538164ed921f23c0
sicula123 <dangngocbich12d3dth2021@gmail.com>
2025-01-10 13:08:36 +0700

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :session, only: [:new, :create, :destroy]
-  resources :registration, only: [:new, :create]
+  resource :session, only: [:new, :create, :destroy]
+  resource :registration, only: [:new, :create]
   resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
