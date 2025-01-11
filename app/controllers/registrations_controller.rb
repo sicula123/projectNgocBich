f432e7bbe60e3e504a83e629f47fd3a9bab6b8ce
sicula123 <dangngocbich12d3dth2021@gmail.com>
2025-01-11 12:03:09 +0700

    fix typo

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,13 @@
 class RegistrationsController < ApplicationController
   allow_unauthenticated_access only: %i[ new create ]
-  before_action :resume_session, only: [:new]
+  before_action :resume_session, only: [ :new ]
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
 
   def new
     redirect_to root_url if authenticated?
   end
 
-  def create 
+  def create
     user = User.new(params.permit(:email_address, :password))
     if user.save
       start_new_session_for user
